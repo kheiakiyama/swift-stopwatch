186bbfbee7fdfe3c8fbe729c1f0aa7663f95e457
kheiakiyama <kheiakiyama@gmail.com>
2015-04-19 23:06:58 +0900

    lesson4

--- StopWatchApp/StopWatchApp/ViewController.swift
+++ StopWatchApp/StopWatchApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var startTime: NSTimeInterval? = nil
+    var timer: NSTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTimer(sender: AnyObject) {
+    }
 
+    @IBAction func stopTimer(sender: AnyObject) {
+    }
+    
+    @IBAction func resetTimer(sender: AnyObject) {
+    }
 }
 
